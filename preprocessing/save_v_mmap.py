8feabae09add41959dee2f2b27e90b1914e28a36
bocchi <131238467+hitorilabs@users.noreply.github.com>
2023-05-22 03:09:20 -0400

    Revert "add repeat testing"
    This reverts commit d7322001d1b3ceb940700b59acaf894dfa49370b.

--- preprocessing/save_v_mmap.py
+++ preprocessing/save_v_mmap.py
@@ -1,16 +1,13 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import pathlib
-import os
 
-source_file = os.getenv("SOURCE_FILE")
-if source_file is None: raise Exception("Missing SOURCE_FILE variable")
-source_path = pathlib.Path(".") / source_file
-data_path = pathlib.Path("datasets") / source_path.stem
+data_path = pathlib.Path("datasets")
 data_path.mkdir(exist_ok=True)
 
 df = pd.read_csv(
-    source_path, 
+    "1958-perceptron/iris.data", 
     header=None,
     names=[
         "sepal_length", 
@@ -25,15 +22,15 @@ df = pd.read_csv(
         "petal_length": np.float32, 
         "petal_width": np.float32, 
         "class": "category",
-    })
+    },
+    engine="pyarrow"
+    )
 
 xs, ys = (
-    np.repeat(df.loc[:, df.columns != "class"].to_numpy(),   1000000), 
-    np.repeat(df["class"].cat.codes.to_numpy(dtype="int64"), 1000000),
+    df.loc[:, df.columns != "class"].to_numpy(), 
+    df["class"].cat.codes.to_numpy()
 )
 
-print(xs.shape)
-print(ys.shape)
 
 np.save(data_path / "train_save.npy", xs)
 np.save(data_path / "target_save.npy", ys)
@@ -41,11 +38,9 @@ np.save(data_path / "target_save.npy", ys)
 rows, *_ = xs.shape
 fp = np.memmap(data_path / "train_memmap.npy", dtype='float32', mode='w+', shape=xs.shape)
 fp[:rows] = xs[:rows]
-print(fp.shape)
 fp.flush()
 
 rows, *_ = ys.shape
 fp = np.memmap(data_path / "target_memmap.npy", dtype="int64", mode='w+', shape=ys.shape)
-print(fp.shape)
 fp[:rows] = ys[:rows]
 fp.flush()
