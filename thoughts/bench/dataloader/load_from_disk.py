fix disk load calculation

thoughts/bench/dataloader/load_from_disk.py
@@ -12,13 +12,11 @@ for _ in range(10):
     
     # Load tensor to CPU memory
     gpu_data = torch.load("batch.pt", map_location=torch.device("cuda:0"))
-    
-    # Copy data to the preallocated space on the GPU
-    preallocated_data.copy_(gpu_data)
-    
+
     end = time.perf_counter()
+
     elapsed = end - start
-    data_size = preallocated_data.nelement() * preallocated_data.element_size()
+    data_size = gpu_data.nelement() * gpu_data.element_size()
     throughput = data_size / elapsed / (1 << 30)
     print(f"completed in {elapsed:.8f}s | {throughput=}GiB/s")
 